569fdf037e9526baa86d0d1d83a451a07557a1ce
boluwatife <owoade.adedamola@yahoo.com>
2020-06-04 09:46:24 +0100

    update to get location of user for maps api

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+const key:string = "AIzaSyDMbJoOSv1cbnHZ2mWOcjIUj4r4GyGF1Pg";
+
+const App: React.FC = () => {
+
+  const [latitude,setLatitude] = useState<number | undefined>(); 
+  const [longitude,setLongitude] = useState<number | undefined>(); 
+  const [data, setData] = useState<any []>();
+  
+  const url = "https://cors.io/?https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=hospital&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&locationbias=circle:2000@"+latitude+","+longitude+"&sensor=false&key="+key
+  const getLocation = ():void => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition( position => {
+        let lat = position.coords.latitude
+        setLatitude(lat)
+        let long = position.coords.longitude
+        setLongitude(long)
+        console.log(latitude)
+        console.log(longitude)
+      });
+    }
+    else {
+      alert("Enable location")
+    }
+  }
+ 
+  // issues with fetch and maps API 
+  const gethospitals = ():void => {
+    fetch(url)
+      .then(response => response.text().then((s) => console.log(JSON.parse(s))))
+      .then(data => console.log(data))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button 
+        className="location"
+        onClick={getLocation}
+      >
+        Get location
+      </button>
+      <button 
+        className="location"
+        onClick={gethospitals}
+      >
+        Get url
+      </button>
     </div>
-  );
+
+  )
 }
 
 export default App;
